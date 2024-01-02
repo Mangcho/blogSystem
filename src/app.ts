426335cf11c17bbe153a9cbb986e8a5614b413b7
Mangcho <bnb1324@naver.com>
2024-01-03 00:32:13 +0900

    feat : swagger 추가 1. app.ts에 swagger 관련 설정 추가, 아직 더 필요 2. swagger.json 설정 파일 추가, 수정필요

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import "./configs/env"
-import express from "express"
+import 'dotenv/config';
+import swaggerUi from 'swagger-ui-express';
+import express from 'express';
+import swaggerDocument from '../swagger.json';
 
 const app = express();
 
+app.use('/api_docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
-app.listen(process.env.SERV_PORT, function(){
-    console.log("Server is running on",process.env.SERV_PORT)
-})
+app.listen(process.env.SERV_PORT, function () {
+    console.log('Server is running on', process.env.SERV_PORT);
+});
